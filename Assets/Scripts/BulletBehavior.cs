6d1a8d3f1d9999a7875a8f5a9e5184e26af501ca
Felix Almqvist <kiwibirdi@hotmail.com>
2019-01-31 11:10:58 +0100

    Felix files Player move and shoot

---
+++ Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    public float m_speed;
+    void FixedUpdate()
+    {
+       GetComponent<Rigidbody2D>().velocity = transform.up * m_speed;
+    }
+
+    void Update()
+    {
+        if(transform.position.y > 5)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
