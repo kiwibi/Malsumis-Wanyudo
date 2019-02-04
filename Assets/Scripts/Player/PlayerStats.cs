4254b350cca2be8c23bd0a6d4708ba6a5a790a12
Jarskih <madcook86@gmail.com>
2019-02-04 13:41:32 +0100

    Health for player

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -6,4 +6,6 @@ public class PlayerStats : MonoBehaviour
 {
     public FloatReference speed;
     public FloatReference pistolCooldown;
+    public IntReference MaxHealth;
+    public IntReference StartingHealth;
 }
