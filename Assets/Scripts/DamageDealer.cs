91ec5ee96142176913a393c5d26dcdf5c929dffd
Felix Almqvist <kiwibirdi@hotmail.com>
2019-02-05 07:15:17 +0100

    Damage dealer
    Made it so that both players and enemies take damage and bullets disapear on collision. also made some changes to player health for easier code

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class DamageDealer : MonoBehaviour
 {
     public IntReference DamageAmount;
+    public GameObject Parent;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Enemy" && Parent.gameObject.tag != "Enemy")
+        {
+            col.gameObject.GetComponent<EnemyStats>().HP -= DamageAmount.Value;
+            Destroy(gameObject);
+        }
+        if(col.gameObject.tag == "Player" && Parent.gameObject.tag != "Player")
+        {
+            col.gameObject.GetComponent<PlayerStats>().Health -= DamageAmount.Value;
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -8,4 +8,13 @@ public class PlayerStats : MonoBehaviour
     public FloatReference pistolCooldown;
     public IntReference MaxHealth;
     public IntReference StartingHealth;
+
+    private int m_Health;
+
+    public int Health { get => m_Health; set => m_Health = value; }
+
+    private void Start()
+    {
+        m_Health = MaxHealth.Value;
+    }
 }
