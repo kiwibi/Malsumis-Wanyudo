e51170f589901b40c6a9cc3afeb69663abdea2bf
Jarskih <madcook86@gmail.com>
2019-02-11 12:11:46 +0100

    Player don't take damage from escaping enemies

--- Assets/Scripts/DestroyGameObject.cs
+++ Assets/Scripts/DestroyGameObject.cs
@@ -12,9 +12,11 @@ public class DestroyGameObject : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(other.gameObject);
+        /*
         if (other.gameObject.tag == "Enemy")
         {
             playerStats.DealDamage(1);
         }
+        */
     }
 }
