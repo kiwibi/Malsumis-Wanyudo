6f270f92adc3c6dec6f4cb55f41bc940c3a6011e
Jarskih <madcook86@gmail.com>
2019-02-11 12:11:22 +0100

    Fireball explodes

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public GameObject Explosion;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Enemy")
+        {
+            Instantiate(Explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+    }
+}
