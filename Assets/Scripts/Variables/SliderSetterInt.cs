8e71a7c2f652bdb4dab58005b3a690fd625f5384
Jarskih <madcook86@gmail.com>
2019-02-11 11:16:22 +0100

    HP bar (placeholder)

---
+++ Assets/Scripts/Variables/SliderSetterInt.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SliderSetterInt : MonoBehaviour
+{
+    public Slider Slider;
+    public IntVariable Variable;
+
+    private void Update()
+    {
+        if (Slider != null && Variable != null)
+            Slider.value = Variable.Value;
+    }
+}
