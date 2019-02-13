3a4c6eda09c75bb202dc5b3782e6864b029d2dd9
Jarskih <madcook86@gmail.com>
2019-02-13 14:37:01 +0100

    Gameover scene

---
+++ Assets/LoseScreenMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseScreenMenu : MonoBehaviour
+{
+    public StringVariable Level1;
+    public StringVariable TitleScreen;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(Level1.Value);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene(TitleScreen.Value);
+    }
+}
