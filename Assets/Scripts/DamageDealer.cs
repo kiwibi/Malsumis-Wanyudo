5ce17d8543069551ae2400a33a24e3c83d098b94
Jari <madcook86@gmail.com>
2019-02-19 15:38:50 +0100

    Fixes to UI, move to boss level when finishing level 3

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -11,7 +11,7 @@ public class DamageDealer : MonoBehaviour
         DamageDealer damageDealer = col.gameObject.GetComponent<DamageDealer>();
         if (damageDealer != null)
         {
-            Debug.Log(damageType.name + " hit: " + damageDealer.damageType.name);
+            //Debug.Log(damageType.name + " hit: " + damageDealer.damageType.name);
             if (damageDealer.damageType.TakesDamageFrom.Contains(damageType))
             {
                 col.gameObject.GetComponent<Stats>().DealDamage(DamageAmount.Value);
