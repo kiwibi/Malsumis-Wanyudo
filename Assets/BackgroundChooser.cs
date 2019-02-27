d4b61ca85c1524aa80f92b9312d603d010deca34
Felix Almqvist <kiwibirdi@hotmail.com>
2019-02-27 14:29:34 +0100

    choosing background

---
+++ Assets/BackgroundChooser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundChooser : MonoBehaviour
+{
+
+    public IntVariable currentLevel;
+    private SpriteRenderer spriteRender;
+    public Sprite[] BGsprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        switch(currentLevel.Value)
+        {
+            case 1:
+                spriteRender.sprite = BGsprites[0];
+                break;
+            case 2:
+                spriteRender.sprite = BGsprites[1];
+                break;
+            case 3:
+                //spriteRender.sprite = BGsprites[2];
+                break;
+        }
+    }
+}
