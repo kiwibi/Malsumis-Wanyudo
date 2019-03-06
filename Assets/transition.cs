10b40416e0a2c0c2b8d8c47232ff5ce11fe87d48
Felix Almqvist <kiwibirdi@hotmail.com>
2019-03-06 10:48:41 +0100

    fading removed

--- Assets/transition.cs
+++ Assets/transition.cs
@@ -9,13 +9,9 @@ public class transition : MonoBehaviour
     public static bool fading;
     public IntVariable currentLevel;
 
-    private flashingText[] fadeEffects;
-
-    public FloatVariable seconds;
     // Update is called once per frame
     void Start()
     {
-        fadeEffects = GetComponentsInChildren<flashingText>();
 
         if (currentLevel == null)
         {
@@ -28,18 +24,13 @@ public class transition : MonoBehaviour
     {
         if (Input.anyKeyDown && fading != true)
         {
-            fading = true;
-            foreach(var fadeEffect in fadeEffects)
-            {
-                fadeEffect.StartFadeOut(seconds.Value);
-            }
             StartCoroutine(changeScene());
         }
     }
 
     IEnumerator changeScene()
     {
-        yield return new WaitForSecondsRealtime(1.5f);
+        yield return new WaitForSecondsRealtime(0.1f);
         fading = false;
         if (currentLevel.Value > 3)
         {
