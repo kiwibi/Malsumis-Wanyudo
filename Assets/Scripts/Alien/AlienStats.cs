b2ff37788e39d4b8cae0d51b22abf242772dc00b
Maohao94 <47109762+Maohao94@users.noreply.github.com>
2019-03-06 14:47:39 +0100

    Some boss balancing

--- Assets/Scripts/Alien/AlienStats.cs
+++ Assets/Scripts/Alien/AlienStats.cs
@@ -6,7 +6,7 @@ public class AlienStats : Stats
     public IntReference AlienMaxHealth;
     public IntVariable AlienHealth;
     private int nextSpeedUp;
-    public readonly int speedUpSteps = 10;
+    public readonly int speedUpSteps = 30;
     private Light lightSource;
 
     private StateController controller;
