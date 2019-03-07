afbfcfe9975393b6f7c19501b338f58d249241aa
Felix Almqvist <kiwibirdi@hotmail.com>
2019-03-07 15:07:13 +0100

    progressbar layer change and heartbeat anim

--- Assets/CorruptedHeart.cs
+++ Assets/CorruptedHeart.cs
@@ -7,19 +7,44 @@ public class CorruptedHeart : MonoBehaviour
 {
     public static CorruptedHeart instance;
 
+    private Animator HeartBeat;
+    public IntVariable CurrentHealth;
 
-    private Image image;
+    private SpriteRenderer heart;
     // Start is called before the first frame update
     void Start()
     {
-        image = GetComponent<Image>();
+        heart = GetComponent<SpriteRenderer>();
+        HeartBeat = GetComponent<Animator>();
         instance = this;
+    }
+
+    void Update()
+    {
+        switch(CurrentHealth.Value)
+        {
+            case 1: case 2:
+                HeartBeat.speed = 5;
+                break;
+            case 3: case 4:
+                HeartBeat.speed = 4;
+                break;
+            case 5: case 6:
+                HeartBeat.speed = 3;
+                break;
+            case 7: case 8:
+                HeartBeat.speed = 2;
+                break;
+            case 9: case 10:
+                HeartBeat.speed = 1;
+                break;
+        }
     }
 
     public static void Darken()
     {
         //var bar = GameObject.Find("UI/Corruption/Fill Area/Fill").GetComponent<Image>();
-        instance.image.color = new Color(instance.image.color.r - 0.01f, instance.image.color.g, instance.image.color.b);
+        instance.heart.color = new Color(instance.heart.color.r - 0.01f, instance.heart.color.g, instance.heart.color.b);
         //bar.color = new Color(bar.color.r, bar.color.g, bar.color.b);
     }
 }
