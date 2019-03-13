4847424b51c848850e09a958f77966ade042ab97
Jari <madcook86@gmail.com>
2019-03-13 17:51:15 +0100

    Boss and player health bar in Boss level

---
+++ Assets/Scripts/FillBossHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FillBossHealth : MonoBehaviour
+{
+    public IntReference maxHealth;
+    public IntReference health;
+    private Image image;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        image.fillAmount = (float)health / (float)maxHealth;
+    }
+}
