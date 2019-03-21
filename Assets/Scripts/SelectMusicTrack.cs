ab4a43db89654338b302819c96fbd6cb3864d066
Felix Almqvist <kiwibirdi@hotmail.com>
2019-03-21 11:56:00 +0100

    music overhaul

--- Assets/Scripts/SelectMusicTrack.cs
+++ Assets/Scripts/SelectMusicTrack.cs
@@ -2,56 +2,121 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Timeline;
+using UnityEngine.SceneManagement;
 
 public class SelectMusicTrack : MonoBehaviour
 {
+    public static SelectMusicTrack instance = null;
+
+    private bool played;
+
     public IntVariable currentLevel;
+    public AudioClip menu;
+    public AudioClip transition;
+    public AudioClip winScreen;
+    public AudioClip loseScreen;
     public AudioClip level1;
     public AudioClip level2;
     public AudioClip level3;
     public AudioClip level4intro;
     public AudioClip level4;
     private AudioSource audioSource;
-    
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
         audioSource = GetComponent<AudioSource>();
-        if (currentLevel.Value == 1)
+        if (instance != null && instance != this)
         {
-            audioSource.clip = level1;
-        } else if (currentLevel.Value == 2)
-        {
-            audioSource.clip = level2;
-        } else if (currentLevel.Value == 3)
-        {
-            audioSource.clip = level3;
-        } else
-        {
-            audioSource.clip = level4intro;
+            Destroy(this.gameObject);
+            return;
         }
-
-        audioSource.Play();
-
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
     }
+    // Start is called before the first frame update
 
     // Update is called once per frame
     void Update()
     {
+        var currentTrack = instance.audioSource.clip;
+        if (SceneManager.GetActiveScene().name == "Level1" || SceneManager.GetActiveScene().name == "BossLevel")
+            selectLevelMusic();
+        else
+            selectMenuMusic();
+
+        //only play clip if its a new one
+        if (currentTrack != instance.audioSource.clip)
+        {
+            instance.audioSource.Play();
+        }
+    }
+
+    void selectMenuMusic()
+    {
+        //check for winScene
+        if(SceneManager.GetActiveScene().name == "WinScene")
+        {
+            instance.audioSource.clip = winScreen;
+            return;
+        }
+        //check for loseScreen
+        if (SceneManager.GetActiveScene().name == "GameOver" || SceneManager.GetActiveScene().name == "BossGameOver")
+        {
+            instance.audioSource.clip = loseScreen;
+            return;
+        }
+        //check for transitionscenes
+        for (int i = 1; i < 4; i++)
+        {
+            var name = "Transition" + i;
+            if(SceneManager.GetActiveScene().name == name)
+            {
+                instance.audioSource.clip = transition;
+                return;
+            }
+        }
+        //if none of the above are true load menu music
+        instance.audioSource.clip = menu;
+    }
+
+    void selectLevelMusic()
+    {
+        //Level 1 > 3
+        if (currentLevel.Value == 1)
+        {
+            instance.audioSource.clip = level1;
+        }
+        else if (currentLevel.Value == 2)
+        {
+            instance.audioSource.clip = level2;
+        }
+        else if (currentLevel.Value == 3)
+        {
+            instance.audioSource.clip = level3;
+        }
+        //boss music
         if (currentLevel.Value > 3)
         {
+            if(instance.played == false)
+            {
+                instance.audioSource.clip = level4intro;
+                instance.played = true;
+            }
             if (audioSource.clip == level4intro)
             {
-                audioSource.loop = false;
-                float trackProgress = audioSource.time / level4intro.length;
+                instance.audioSource.loop = false;
+                float trackProgress = instance.audioSource.time / level4intro.length;
 
                 if (trackProgress > 0.99f)
                 {
-                    audioSource.clip = level4;
-                    audioSource.loop = true;
-                    audioSource.Play();
-                }  
+                    instance.audioSource.clip = level4;
+                    instance.audioSource.loop = true;
+                    instance.audioSource.Play();
+                }
             }
         }
-    }
+    }   
 }
