9f65b1e23f39cd41b893328b11e49fe1e12d717f
Jarskih <madcook86@gmail.com>
2019-03-21 13:48:13 +0100

    Fireball sound fix

---
+++ Assets/Scripts/SoundEvents.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class SoundEvents : MonoBehaviour
+{
+    public SimpleAudioEvent fireballSound;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    void OnEnable()
+    {
+        EventManager.StartListening("Fireball", FireBallSound);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("Fireball", FireBallSound);
+    }
+
+    void FireBallSound()
+    {
+        fireballSound.Play(audioSource);
+    }
+}
