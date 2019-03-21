a12fdb4ce14c1795902f502a228d239cd8d3b831
Jarskih <madcook86@gmail.com>
2019-03-21 14:54:14 +0100

    Explosion spawns smoke

---
+++ Assets/DisableAfterSeconds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableAfterSeconds : MonoBehaviour
+{
+    public float disableAfterSeconds;
+    void Start()
+    {
+        StartCoroutine("Disable");
+    }
+
+    IEnumerator Disable()
+    {
+        yield return new WaitForSeconds(disableAfterSeconds);
+        gameObject.GetComponentInChildren<SpriteRenderer>().enabled = false;
+        gameObject.GetComponentInChildren<DamageDealer>().enabled = false;
+        gameObject.GetComponentInChildren<CircleCollider2D>().enabled = false;
+    }
+}
---
+++ Assets/SpawnSmoke.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSmoke : MonoBehaviour
+{
+    private ParticleSystem particles;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles = GetComponentInChildren<ParticleSystem>();
+        particles.Play();
+    }
+}
