47f7c1f69e0bf678d2f0179b5ee595d22818f406
Jarskih <madcook86@gmail.com>
2019-03-27 12:03:13 +0100

    Startlight for oscar

---
+++ Assets/Scripts/StarLight.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarLight : MonoBehaviour
+{
+    private Light light;
+    public int minIntensity;
+    public int maxIntensity;
+    public bool intensityUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if intensityUp true
+        // if intensity is smaller than maxIntensity
+        // make intensity bigger
+
+        // if intensity is bigger than maxIntensity
+        // change instensityUp to false
+
+        // if intensityUp false
+        // do same thing other way
+    }
+}
