8ddb19f34e21189fd4af0125eadfacdecbaecd98
Felix Almqvist <kiwibirdi@hotmail.com>
2019-03-27 11:47:35 +0100

    dynamic progressbar & new intro picture

--- Assets/Scripts/FillSprite.cs
+++ Assets/Scripts/FillSprite.cs
@@ -9,16 +9,32 @@ public class FillSprite : MonoBehaviour
 
     private float checkPoint1;
     private float checkPoint2;
+    private float multiplier;
     //private float checkPoint3;
 
     public IntVariable Currentlevel;
     public IntVariable CurrentKillCount;
+    public IntVariable Level1Clear;
+    public IntVariable Level2Clear;
+    public IntVariable Level3Clear;
 
     void Start()
     {
         fillArea = GetComponent<Image>();
         checkPoint1 = 0.29f;
         checkPoint2 = 0.57f;
+        switch(Currentlevel.Value)
+        {
+            case 1:
+                multiplier = checkPoint1 / Level1Clear.Value;
+                break;
+            case 2:
+                multiplier = checkPoint2 / Level2Clear.Value;
+                break;
+            case 3:
+                multiplier = 1.0f / Level3Clear.Value;
+                break;
+        }
 
     }
 
@@ -28,13 +44,13 @@ public class FillSprite : MonoBehaviour
         {
             case 1:
 
-                fillArea.fillAmount = CurrentKillCount.Value * 0.0058f;
+                fillArea.fillAmount = CurrentKillCount.Value * multiplier;
                 break;
             case 2:
-                fillArea.fillAmount = checkPoint1 + ((CurrentKillCount.Value - 50) * 0.0056f);
+                fillArea.fillAmount = checkPoint1 + ((CurrentKillCount.Value - Level1Clear.Value) * multiplier);
                 break;
             case 3:
-                fillArea.fillAmount = checkPoint2 + ((CurrentKillCount.Value - 100) * 0.0086f);
+                fillArea.fillAmount = checkPoint2 + ((CurrentKillCount.Value - Level2Clear.Value) * multiplier);
                 break;
         }
     }
