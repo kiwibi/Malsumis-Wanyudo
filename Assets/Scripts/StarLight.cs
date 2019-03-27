d1383766f0108f8ff07a57acc050319fd469cb05
OscarioTheGuy <oscarsvan@hotmail.com>
2019-03-27 12:48:24 +0100

    Lights

--- Assets/Scripts/StarLight.cs
+++ Assets/Scripts/StarLight.cs
@@ -8,6 +8,7 @@ public class StarLight : MonoBehaviour
     private Light light;
     public int minIntensity;
     public int maxIntensity;
+    public float intensitySpeed;
     public bool intensityUp;
 
     // Start is called before the first frame update
@@ -19,7 +20,22 @@ public class StarLight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if intensityUp true
+        if (intensityUp == true){
+            light.intensity = light.intensity + Time.deltaTime*intensitySpeed;
+        }
+        if (light.intensity < minIntensity)
+        {
+            intensityUp = true;
+        }
+        if (light.intensity > maxIntensity)
+        {
+            intensityUp = false;
+        }
+        if (intensityUp == false)
+        {
+            light.intensity = light.intensity - Time.deltaTime*intensitySpeed;
+        }
+            
         // if intensity is smaller than maxIntensity
         // make intensity bigger
 
